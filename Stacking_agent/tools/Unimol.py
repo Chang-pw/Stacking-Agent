9230ce18a1a5cd0cd31f2d631bc52c0bbbc931c2
ZhijianZhou <1275747829@qq.com>
2025-02-10 05:49:43 +0000

    test

--- Stacking_agent/tools/Unimol.py
+++ Stacking_agent/tools/Unimol.py
@@ -5,6 +5,7 @@ import pickle
 import pandas as pd
 import argparse
 from rdkit import Chem
+import json
 
 from unimol_tools import MolTrain, MolPredict
 import os
@@ -72,21 +73,48 @@ def get_data(file_path):
         target_type = "classification"
     return train_csv_path, test_csv_path, target_type
 
+def get_data_json(file_path):
+    label_dict = {}
+    label_dict["Yes"] = 1
+    label_dict["No"] = 0
+    with open(os.path.join(file_path,"test.json"),"r") as fp:
+        data = json.load(fp)
+    result = []
+    for i in data:
+        mol = {}
+        if Chem.MolFromSmiles(i['SMILES'])is None:
+            print("wrong smiles")
+            continue
+        mol["SMILES"] = i['SMILES']
+        mol["TARGET"] = label_dict[i['gold_answer']]
+        result.append(mol)
+    df = pd.DataFrame(result)
+    df.to_csv(os.path.join(file_path,"test.csv"), index=False)
+    return os.path.join(file_path,"test.csv")
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="train_unimol")
-    parser.add_argument('--file_path', type=str, help="read_path",default="../../Dataset/molecular_property_prediction/hiv")
+    parser.add_argument('--file_path', type=str, help="read_path",default="../../Dataset/MolecularPropertyPrediction/bbbp")
+    parser.add_argument('--task', type=str, help="train or test",default="test")
+    parser.add_argument('--model_path', type=str, help="where your finetuned model")
     args = parser.parse_args()
     file_path = args.file_path
-    train_csv_path, test_csv_path, target_type = get_data(file_path)
-    print(target_type)
-    clf = MolTrain(task= target_type, #multiclass #classification # multilabel_classification
-                    data_type='molecule', 
-                    epochs=10, 
-                    batch_size=16, 
-                    metrics='auc',
-                    model_name='unimolv2', # avaliable: unimolv1, unimolv2
-                    model_size='84m', # work when model_name is unimolv2. avaliable: 84m, 164m, 310m, 570m, 1.1B.
-                    )
-    pred = clf.fit(data = train_csv_path )
-    clf = MolPredict(load_model='../exp')
-    res = clf.predict(data = test_csv_path)
+    task = args.task
+    if task == "train":
+        train_csv_path, test_csv_path, target_type = get_data(file_path)
+
+        clf = MolTrain( task= target_type, #multiclass #classification # multilabel_classification
+                        data_type='molecule', 
+                        epochs=10, 
+                        batch_size=16, 
+                        metrics='auc',
+                        model_name='unimolv2', # avaliable: unimolv1, unimolv2
+                        model_size='84m', # work when model_name is unimolv2. avaliable: 84m, 164m, 310m, 570m, 1.1B.
+                        save_path = f'./exp_{file_path.split("/")[-1]}'
+                        )
+        pred = clf.fit(data = train_csv_path )
+        clf = MolPredict(load_model=f'./exp_{file_path.split("/")[-1]}')
+        res = clf.predict(data = test_csv_path)
+    elif task == "test":
+        test_csv_path = get_data_json( file_path )
+        clf = MolPredict(load_model=args.model_path)
+        res = clf.predict(data = test_csv_path)
